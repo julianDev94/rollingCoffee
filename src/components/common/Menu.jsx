7230334a2e0070270d8563e31a07de40abfbeddc
JulianAballay <juliancabj1210@gmail.com>
2024-02-25 21:33:03 -0300

    agregamos react router a los elementos del menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -2,12 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../assets/Coffee_Logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container className="d-flex justify-content-around">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to={"/"} >
           <img
             src={logo}
             alt="Imagen de logo rolling coffee"
@@ -19,7 +20,7 @@ const Menu = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto ">
-              <Nav.Link href="#home">Inicio</Nav.Link>
+              <NavLink end className="nav-link" to="/">Inicio</NavLink>
               <Nav.Link href="#link">Administrador</Nav.Link>
               <Nav.Link href="#link">Registro</Nav.Link>
               <Nav.Link href="#link">Login</Nav.Link>
