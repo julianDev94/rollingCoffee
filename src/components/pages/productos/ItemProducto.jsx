622066e32e3c05a91250deab1f299ae1785e7696
JulianAballay <juliancabj1210@gmail.com>
2024-02-27 15:18:16 -0300

    cambios

--- src/components/pages/productos/ItemProducto.jsx
+++ src/components/pages/productos/ItemProducto.jsx
@@ -11,7 +11,12 @@ const ItemProducto = () => {
       </td>
       <td>Bebida caliente</td>
       <td>
-        <i class="bi bi-trash"></i>
+        <a href="" className="btn btn-danger mx-2">
+          <i className="bi bi-trash"></i>
+        </a>
+        <a href="" className="btn btn-warning">
+          <i className="bi bi-pencil-square"></i>
+        </a>
       </td>
     </tr>
   );
