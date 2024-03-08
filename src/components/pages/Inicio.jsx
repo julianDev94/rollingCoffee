7862002bfa68a881a0c6aeffc6f4b0e88b4c9471
JulianAballay <juliancabj1210@gmail.com>
2024-03-07 21:21:29 -0300

    agrego logica de las card en el inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,8 +1,27 @@
 import imgBanner from "../../assets/banner.png";
 import { Row, Col, Container } from "react-bootstrap";
 import CardProducto from "./productos/CardProducto";
+import { leerProductosAPI } from "../../helpers/queries";
+import { useEffect, useState } from "react";
 
 const Inicio = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    consultarProductos();
+  }, []);
+
+  const consultarProductos = async () => {
+    try {
+      const listaProductos = await leerProductosAPI();
+      console.log("dentro del metodo consultar producto");
+      //console.log(listaProductos);
+      setProductos(listaProductos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="seccionMain">
       <img
@@ -13,9 +32,11 @@ const Inicio = () => {
       <Container className="my-3">
         <h1 className="display-2">Nuestros Productos</h1>
         <Row xs={1} md={3} className="g-4">
-          {Array.from({ length: 4 }).map((_, idx) => (
+          {Array.from({ length: productos.length }).map((_, idx) => (
             <Col key={idx}>
-              <CardProducto />
+              {productos.map((producto) => (
+                <CardProducto producto={producto} key={producto.id} />
+              ))}
             </Col>
           ))}
         </Row>
--- src/components/pages/productos/CardProducto.jsx
+++ src/components/pages/productos/CardProducto.jsx
@@ -1,18 +1,18 @@
 import { Card, Button } from "react-bootstrap";
 import imagenCafecito from "../../../assets/cafecitoAmericano.jpg";
 
-const CardProducto = () => {
+const CardProducto = ({ producto }) => {
   return (
     <Card className="mx-1 w-100">
-      <Card.Img variant="top" src={imagenCafecito} />
+      <Card.Img variant="top" src={producto.imagen} />
       <Card.Body>
-        <Card.Title>Tipo de café</Card.Title>
+        <Card.Title>{producto.nombreProducto}</Card.Title>
         <Card.Text>
-          Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore ratione culpa libero reprehenderit commodi nesciunt vitae. Minus unde, cumque vel quos reprehenderit similique aliquid ullam in quas. Dolore, perspiciatis.
+          {producto.descripcionBreve}
         </Card.Text>
-        <Card.Text className="fw-bold">Precio: $100</Card.Text>
+        <Card.Text className="fw-bold">Precio: ${producto.precio}</Card.Text>
         <Card.Footer className="d-flex justify-content-end">
-            <Button variant="success">Ver más</Button>
+          <Button variant="success">Ver más</Button>
         </Card.Footer>
       </Card.Body>
     </Card>
